7cbe74a03e6e96ba7786fc4b6a21dffae759fd6a
Eason Lu <aelnosu@outlook.com>
2023-10-10 16:18:21 -0700

    Create 1.cpp
    Signed-off-by: Eason Lu <aelnosu@outlook.com>

---
+++ 1.cpp
@@ -0,0 +1,44 @@
+/*
+(3 4 5) => 6
+(15 16 17) => 120
+(20 21 22) => 210
+(44 45 46) => 990
+(608 609 610) => 185136
+(3496 3497 3498) => 6112756
+*/
+#include <iostream>
+
+using namespace std;
+
+int trinum(int n){
+    return (n * (n + 1)) / 2;
+}
+
+bool conseint(int num) {
+    for (int i = 1; i <= num / 3; i++) {
+        if (i * (i + 1) * (i + 2) == num) {
+            return true;
+        }
+    }
+    return false;
+}
+int iconseint(int num) {
+    for (int i = 1; i <= num / 3; i++) {
+        if (i * (i + 1) * (i + 2) == num) {
+            return i;
+        }
+    }
+    return -1;
+}
+int main() {
+    printf("(");
+    for (int x = 0; trinum(x) <= 258474216; x++) {
+        if (conseint(trinum(x))) {
+            int y = iconseint(trinum(x));
+            int z = trinum(x);
+            printf("%d %d %d",x,y,z);
+        }
+    }
+    printf(")");
+    return 0;
+}
